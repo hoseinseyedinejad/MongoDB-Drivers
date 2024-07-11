fa99c293f1587f7a8d4dcac00613f750b897511d
Hosein SeyediNejad <Hosein.seyedinejad@gmail.com>
2024-07-11 11:08:35 +0330

    aggregation pipleline stages added

--- Program.cs
+++ Program.cs
@@ -1,14 +1,13 @@
 ﻿
-using System.Text.Json;
 using MongoDB.Driver;
 using MongoDB.Bson;
-using System.Security.Cryptography.X509Certificates;
 
 var MongdbUrl = "mongodb+srv://hoseinseyedinejad:FallOf68@myfirstcluster.90tholj.mongodb.net/?retryWrites=true&w=majority&appName=MyFirstCluster";
 var client = new MongoClient(MongdbUrl);
 var database = client.GetDatabase("blog");
 
-//CRUD Using Bson Document
+#region CRUD Using Bson Document
+#region using BsonDocument
 var postsInBsonDoc = database.GetCollection<BsonDocument>("posts");
 // var newBsonPost = new BsonDocument
 // {
@@ -24,10 +23,9 @@ var postsInBsonDoc = database.GetCollection<BsonDocument>("posts");
 //     .Eq("likes", 336);
 // var BsonPost = postsInBsonDoc.Find(filter).FirstOrDefault();
 // Console.WriteLine(BsonPost);
+#endregion
 
-
-// CRUD Using C# Class
-
+#region using cSharpClasses
 //Insert
 var postsInCSharpClass = database.GetCollection<BlogPost>("posts");
 
@@ -55,41 +53,92 @@ var postsInCSharpClass = database.GetCollection<BlogPost>("posts");
 // if(updateRes.IsAcknowledged){
 //     Console.WriteLine($"Matched:{updateRes.MatchedCount}; Modified:{updateRes.ModifiedCount};");
 // }
+#endregion
+#endregion
 
-//Create new Collection
+#region Create new Collection and using of transaction in multiple updates
 //database.CreateCollection("LikeShares");
 
 var likeSharesInCSharpClass = database.GetCollection<LikeShare>("LikeShares");
-// using transaction
-using (var session = client.StartSession())
+// using (var session = client.StartSession())
+// {
+//     var fromId = "668e978a8378966655008f26";
+//     var toId = "668ea2e19948e39b538f89f5";
+
+//     var shareAmount = 375;
+
+//     var fromPostResult = postsInCSharpClass.Find(x => x.Id == fromId).FirstOrDefault();
+//     var toPostResult = postsInCSharpClass.Find(x => x.Id == toId).FirstOrDefault();
+
+//     var share = new LikeShare
+//     {
+//         FromPost = "668e978a8378966655008f26",
+//         ToPost = "668ea2e19948e39b538f89f5",
+//         sharedAmount = shareAmount
+//     };
+
+//     var fromPostFilter = Builders<BlogPost>.Filter.Eq(x => x.Id, fromId);
+//     var toPostFilter = Builders<BlogPost>.Filter.Eq(x => x.Id, toId);
+//     var fromPostUpdate = Builders<BlogPost>.Update.Set(x => x.Likes, fromPostResult.Likes - shareAmount);
+//     var toPostUpdate = Builders<BlogPost>.Update.Set(x => x.Likes, toPostResult.Likes + shareAmount);
+//     var fromPostUpdateResult = postsInCSharpClass.UpdateOne(fromPostFilter, fromPostUpdate);
+//     var toPostUpdateResult = postsInCSharpClass.UpdateOne(toPostFilter, toPostUpdate);
+//     likeSharesInCSharpClass.InsertOne(share);
+
+
+//     Console.WriteLine(JsonSerializer.Serialize(fromPostUpdateResult));
+//     Console.WriteLine(JsonSerializer.Serialize(toPostUpdateResult));
+//     Console.WriteLine(JsonSerializer.Serialize(likeSharesInCSharpClass.Find(_ => true).FirstOrDefault()));
+// }
+#endregion
+
+#region using aggregate pipleline
+#region using BsonDocument
+
+// var matchStage = Builders<BsonDocument>.Filter.Lte("likes", 10);
+
+// var aggregate = postsInBsonDoc.Aggregate().Match(matchStage);
+
+// foreach (var post in aggregate.ToList())
+// {
+//     Console.WriteLine(post["likes"]);
+// }
+#endregion
+
+#region using c#
+//match stage
+var matchStage = Builders<BlogPost>.Filter.Lte(x => x.Likes, 10);
+
+var aggregate = postsInCSharpClass.Aggregate().Match(matchStage);
+
+foreach (var post in aggregate.ToList())
 {
-    var fromId = "668e978a8378966655008f26";
-    var toId = "668ea2e19948e39b538f89f5";
-
-    var shareAmount = 375;
-
-    var fromPostResult = postsInCSharpClass.Find(x => x.Id == fromId).FirstOrDefault();
-    var toPostResult = postsInCSharpClass.Find(x => x.Id == toId).FirstOrDefault();
-
-    var share = new LikeShare
-    {
-        FromPost = "668e978a8378966655008f26",
-        ToPost = "668ea2e19948e39b538f89f5",
-        sharedAmount = shareAmount
-    };
-
-    var fromPostFilter = Builders<BlogPost>.Filter.Eq(x => x.Id, fromId);
-    var toPostFilter = Builders<BlogPost>.Filter.Eq(x => x.Id, toId);
-    var fromPostUpdate = Builders<BlogPost>.Update.Set(x => x.Likes, fromPostResult.Likes - shareAmount);
-    var toPostUpdate = Builders<BlogPost>.Update.Set(x => x.Likes, toPostResult.Likes + shareAmount);
-    var fromPostUpdateResult = postsInCSharpClass.UpdateOne(fromPostFilter, fromPostUpdate);
-    var toPostUpdateResult = postsInCSharpClass.UpdateOne(toPostFilter, toPostUpdate);
-    likeSharesInCSharpClass.InsertOne(share);
-
-
-    Console.WriteLine(JsonSerializer.Serialize(fromPostUpdateResult));
-    Console.WriteLine(JsonSerializer.Serialize(toPostUpdateResult));
-    Console.WriteLine(JsonSerializer.Serialize(likeSharesInCSharpClass.Find(_ => true).FirstOrDefault()));
+    Console.WriteLine(post.Likes);
 }
 
+//match and group stage
+var groupAggregate = postsInCSharpClass.Aggregate().Match(matchStage).Group(x => x.Category, r => new { category = r.Key, total = r.Sum(a => 1) });
+
+foreach (var group in groupAggregate.ToList())
+{
+    Console.WriteLine(group);
+}
+
+//sort stage
+var projectionStage = Builders<BlogPost>.Projection.Expression(p =>
+    new
+    {
+        Title = p.Title,
+        Likes = p.Likes
+    });
+
+//var sortAggregate = postsInCSharpClass.Aggregate().SortByDescending(x => x.Likes).Project(p => new { p.Title, p.Likes });
+var sortAggregate = postsInCSharpClass.Aggregate().SortByDescending(x => x.Likes).Project(projectionStage);
+var sortedProjections = sortAggregate.ToList();
+foreach (var sp in sortedProjections)
+{
+    Console.WriteLine(sp);
+}
+#endregion
+#endregion
 
